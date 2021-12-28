a4f36750853fd24ef0873205dff4484f1289ffdf
Lawrimon <inf20146@lehre.dhbw-stuttgart.de>
2021-12-28 11:41:02 +0100

    Initial Commit

---
+++ Hockeylitics/HockeyliticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyliticsApp.swift
+//  Hockeylitics
+//
+//  Created by Laurin Tarta on 28.12.21.
+//
+
+import SwiftUI
+
+@main
+struct HockeyliticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
